a54c53f3f01597a6615f44b1f8a9908b49f044f6
cranci1 <100066266+cranci1@users.noreply.github.com>
2025-08-15 09:27:33 +0200

    ops

--- Emby/Emby.js
+++ Emby/Emby.js
@@ -15,11 +15,11 @@
 // Make sure to host this file on a private webserver or a private GitHub repository!
 //
 
----
+// Settings start
 const baseUrl = "http://localhost:8096"; // e.g., "http://192.168.0.1:8096";
 const apiKey = "key"; // Your Emby API key
 const userId = "userID"; // Your Emby User ID (can be found in Settings > Users, in the URL)
----
+// Settings end
 
 //
 // Do not edit below this line unless you know what you're doing.
